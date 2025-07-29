25a0540f31e7bdb1377010975ebfb65a6c7be544
Viraj5138 <viraj38t@gmail.com>
2025-07-29 23:40:28 +0530

    Initial clean commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// console.log("Hello Reeek!!");
+// var message;
+
+// message = ("FCUK OUY");
+// console.log(message);
+
+// var me;
+// me= ("viraj");
+// me= ("fkljf");
+// console.log(me)
+
+// let V;
+// V = ("VIrja");
+// let V = ("kgjdfk");
+// console.log(V);
+
+// const v= ("fuck you");
+// console.log(v);
+
+// let isProductInCart = false;
+// console.log(typeof isProductInCart);
+
+// let userName = "Shrek";
+// let color = "Green";
+
+// let message = "My name is " + userName + " and I am " + color + " in color";
+// console.log(message);
+
+// let text = `My Name is ${userName} and I am ${color} in color`;
+// console.log(text);
+
+// const readlineSync = require("readline-sync");
+
+// const Username = readlineSync.question("Waht's your name?")
+
+// const userAge = readlineSync.question("how old are you?");
+
+// const birthYear = 2025 + Number(userAge);
+
+// console.log(`what the is that name ${Username} !!!`);
+
+// console.log(`my grandma was born in ${birthYear} too....you old.`)
+
+// const readlineSync = require("readline-sync");
+
+// const number = Number(readlineSync.question("enter number..."));
+
+//  if (number % 5 ===0 && number % 3 ===0) {
+//     console.log("fizz");
+// }else if (number % 5 ===0 || number % 3 ===0) {
+//     console.log("buzz");
+// }
+// else {
+//     console.log("fuck!");
+// }
+
+
+const readlineSync = require("readline-sync")
+// const Sec  = require("readline-sync");
+// const Third = require("readline-sync");
+
+// const first = readlineSync.question("enter string 1: ");
+// const second = readlineSync.question("enter string 2: ");
+// const third = readlineSync.question("enter 3rd string: ");
+
+// if (first.length < second.length && first.length < third.length) {
+//     console.log("string 1 is smalllest ");
+// }
+
+// if (second.length < first.length && second.length < third.length) {
+//     console.log("string 2 is smallest");
+// }
+
+// if (third.length < first.length && third.length < second.length) {
+//     console.log("string 3 is smallest");
+// }
+// else {
+//     console.log("Found 2 or more strings of the same lenght");
+// }
+
+const TotalMarks = 59;
+
+// (TotalMarks <45) ? console.log("You failed") : console.log("You Passed")
+
+// const Result = TotalMarks < 60 ? "You failure" : "YOu not failingg"
+
+// console.log(Result);
+
+// const result = TotalMarks < 40 ? "work hard" : TotalMarks < 60 ? "B" : TotalMarks < 75 ? "A" : TotalMarks < 85 ? "A+" : "Genius";
+
+// console.log(result);
+
+// const firstName = "viraj" ;
+// const nickName = "";
+
+// const userName = firstName || nickName || "NoName" ;
+// console.log(`Name - ${userName}`);
+
+// const userName = firstName && nickName;
+// console.log(`Name - ${userName}`);
+
+let firstname;
+console.log(firstname ?? "hiddenGeek");
