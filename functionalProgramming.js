a60341d83ae3b903900470f9bebd28b3e602db9f
Viraj5138 <viraj38t@gmail.com>
2025-08-20 11:52:24 +0530

    Functional Programming Completed

--- functionalProgramming.js
+++ functionalProgramming.js
@@ -1,5 +1,80 @@
-function greetMessage() {
-    console.log("helllo ")
+// function greetMessage(name) {
+//     console.log(`${name}, welcome to gfg!!`);
+// }
+
+// greetMessage("Viraj");
+// greetMessage("John");
+
+// let anonoymousFunction = function () {
+//     console.log("I am anonymous");
+// }
+
+// anonoymousFunction();
+
+// function calculate(x,y) {
+//    return(x+y);
+// }
+
+// console.log(calculate(12,45));
+
+// const result = calculate(345,545454);
+// console.log(result);
+
+
+// const calculateAdd = (x,y) => x+y
+
+// console.log(calculateAdd(12,45));
+
+// const calSum = (x,y) => {
+//     if (x>y) {
+//         return x+y
+//     } 
+//     else {
+//         return x-y
+//     }
+// }
+
+// console.log(calSum(34,23));
+
+// const sum = (x,y) => x>y ? x+y : x-y;
+// const output = sum(34,655);
+// console.log(output);
+
+// function sumofN(start,end) {
+//     let sum = 0
+//     for(let i = start; i<=end; i++) {
+//         sum = sum+ i;
+//     }
+//     return sum;
+// }
+
+// const result = sumofN(34,54);
+// console.log(result); 
+
+// const studentIds = new Set();
+
+// studentIds.add(1);
+// studentIds.add(2);
+// studentIds.add(1);
+
+// console.log({studentIds})
+
+const stds = [1,2,3,4,5,6,4,4,];
+console.log(stds);
+const ustds = new Set(stds);
+console.log(ustds);
+
+const nstds = [...ustds]
+console.log({nstds})
+
+
+function Sum(start, end) {
+    let sum = 0;
+    for(let i =start ; i<=end ; i++) {
+        sum = sum + i ;
+
+    }
+    return sum;
 }
 
-greetMessage() 
+console.log(Sum (1,5)) ;
